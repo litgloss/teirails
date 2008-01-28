f4acf5237e8fd3c27ffb03896544e429e2b0131f
Justin S. Leitgeb <leitgeb@buffalo.edu>
2008-01-28 08:59:45 -0500

    Adding new files for user authentication, etc.

---
+++ db/migrate/002_create_contents.rb
@@ -0,0 +1,13 @@
+class CreateContents < ActiveRecord::Migration
+  def self.up
+    create_table :contents, :force => true do |t|
+      t.text :teidata
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contents
+  end
+end
